429f12f35d3ecf6fc6b34acd4093949e9e5ca8cc
Alison Bandeira <alisonlimabandeira@gmail.com>
2026-03-22 18:15:21 -0500

    pacinete

--- src/main/java/com/consulta/model/PacienteConvidado.java
+++ src/main/java/com/consulta/model/PacienteConvidado.java
@@ -2,22 +2,15 @@ package com.consulta.model;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import lombok.Getter;
@@ -39,9 +32,6 @@ public class PacienteConvidado implements Serializable {
 	
 	@Column(name = "nome")
 	private String nome;
-	
-	@Column(name = "username")
-	private String username;
 	
 	@Column(name = "telefone")
 	private String telefone;
